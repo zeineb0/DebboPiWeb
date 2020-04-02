4ba7c58ada4a86d066bd044269144269b38fd113
TaherZ <taher.zoghlami1b@gmail.com>
2020-04-02 19:01:50 +0100

    Creation du bundle RH et FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new EntrepotBundle\EntrepotBundle(),
+            new RHBundle\RHBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
